b4d51d52dfd3ecc4917830c7d8e03014b20b48c2
pekellogg <pekellogg@hotmail.com>
2021-05-11 19:02:40 -0700

    commit tree c801514e9c687882fa6cee027b5dd4a1fe494d2c

--- lib/car.rb
+++ lib/car.rb
@@ -1,1 +1,10 @@
 require_relative "./vehicle.rb"
+
+# Since there are two instance methods, #go, the compiler looks inside the class on which #go was
+# called first, THEN in the super class. In this case, it's executing #go from the Car class.
+
+class Car < Vehicle
+    def go
+        "VRRROOOOOOOOOOOOOOOOOOOOOOOM!!!!!"
+    end
+end
